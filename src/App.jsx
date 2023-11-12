b75515ebcbae770a59d3a8e3bd30fb4a8de04037
Mengyuan Li <mli114@volvocars.com>
2023-11-12 18:00:59 +0100

    added holding logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,52 @@
 import "./App.css";
 import Die from "./Die";
 import React from "react";
+import { nanoid } from "nanoid";
 
 function App() {
 	const [dice, setDice] = React.useState(allNewDice());
 
 	function allNewDice() {
-		return Array.from(
-			{ length: 10 },
-			() => Math.floor(Math.random() * 6) + 1
-		);
+		return Array.from({ length: 10 }, () => {
+			return {
+				value: Math.floor(Math.random() * 6) + 1,
+				isHeld: false,
+				id: nanoid(),
+			};
+		});
 	}
 
 	function handleRoll() {
-		setDice((oldDice) =>
-			oldDice.map((d) =>
-				d.isFrozen ? d : Math.floor(Math.random() * 6) + 1
-			)
-		);
+		setDice((oldDice) => {
+			return oldDice.map((d) => {
+				if (d.isHeld) return d;
+				return { ...d, value: Math.floor(Math.random() * 6) + 1 };
+			});
+		});
 	}
 
-	const diceElements = dice.map((d) => <Die value={d} />);
+	function holdDice(id) {
+		setDice((oldDice) => {
+			return oldDice.map((d) => {
+				return d.id === id ? { ...d, isHeld: !d.isHeld } : d;
+			});
+		});
+	}
+
+	const diceElements = dice.map((d) => (
+		<Die
+			key={d.id}
+			id={d.id}
+			value={d.value}
+			isHeld={d.isHeld}
+			holdDice={() => holdDice(d.id)}
+		/>
+	));
 
 	return (
 		<main>
-			<h1>Tenzies</h1>
-			<p>
+			<h1 className="title">Tenzies</h1>
+			<p className="instructions">
 				Roll until all dice are the same. Click each die to freeze it at
 				its current value between rolls.
 			</p>
