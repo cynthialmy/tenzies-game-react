3fd007fd15e338405468d33a8cac00adc64efedf
Mengyuan Li <mli114@volvocars.com>
2023-11-12 10:13:07 +0100

    added dice rolling function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import "./App.css";
 import Die from "./Die";
+import React from "react";
 
 function App() {
+	const [dice, setDice] = React.useState(allNewDice());
+
+	function allNewDice() {
+		return Array.from(
+			{ length: 10 },
+			() => Math.floor(Math.random() * 6) + 1
+		);
+	}
+
+	function handleRoll() {
+		setDice((oldDice) =>
+			oldDice.map((d) =>
+				d.isFrozen ? d : Math.floor(Math.random() * 6) + 1
+			)
+		);
+	}
+
+	const diceElements = dice.map((d) => <Die value={d} />);
+
 	return (
 		<main>
 			<h1>Tenzies</h1>
@@ -9,19 +29,10 @@ function App() {
 				Roll until all dice are the same. Click each die to freeze it at
 				its current value between rolls.
 			</p>
-			<div className="dice-container">
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-				<Die value="1" />
-			</div>
-			<button className="btn btn-danger">Roll!</button>
+			<div className="dice-container">{diceElements}</div>
+			<button className="button-roll" onClick={handleRoll}>
+				Roll!
+			</button>
 		</main>
 	);
 }
